c6c29cc564f650f22b2b25e7f754a74aa6eb3485
BimalKanxa <sharmabimal2003@gmail.com>
2023-08-06 20:55:35 +0530

    all features working fine till here

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,85 +1,78 @@
 
-const User = require('../models/userModel');
-const ErrorResponse = require('../utils/errorResponse');
+const User= require('../models/userModel')
+const ErrorResponse = require('../utils/errorResponse')
 
-
-exports.signup = async (req, res, next) => {
-    const { email } = req.body;
-    const userExist = await User.findOne({ email });
-    if (userExist) {
-        return next(new ErrorResponse("E-mail already registred", 400));
+exports.signup = async (req, res, next) =>{
+    const {email} = req.body;
+    const userExist = await User.findOne({email})
+    if(userExist){
+        return next(new ErrorResponse("Email-id already registered", 400));
     }
-    try {
+    try{
         const user = await User.create(req.body);
         res.status(201).json({
-            success: true,
+            success : true,
             user
         })
-    } catch (error) {
-        next(error);
+    }
+    catch(error){
+            next(error);
     }
 }
 
 
-exports.signin = async (req, res, next) => {
+exports.signin = async (req, res, next) =>{
+    try{
+    const {email, password} = req.body;
 
-    try {
-        const { email, password } = req.body;
-        //validation
-        if (!email) {
-            return next(new ErrorResponse("please add an email", 403));
+    //validation
+        if(!email){
+            return next(new ErrorResponse("Please add an email", 400));
         }
-        if (!password) {
-            return next(new ErrorResponse("please add a password", 403));
+        if(!password){
+            return next(new ErrorResponse("Please add password", 403));
         }
-
         //check user email
-        const user = await User.findOne({ email });
-        if (!user) {
-            return next(new ErrorResponse("invalid credentials", 400));
+        const user = await User.findOne({email})
+        if(!user){
+            return next(new ErrorResponse("Invalid Credentials", 400));
         }
         //check password
-        const isMatched = await user.comparePassword(password);
-        if (!isMatched) {
-            return next(new ErrorResponse("invalid credentials", 400));
+        const isMatched = user.comparePassword(password)
+        if(!isMatched){
+            return next(new ErrorResponse("Invalid Credentials", 400));
         }
-
         sendTokenResponse(user, 200, res);
-
-    } catch (error) {
-        next(error);
+    }
+    catch(error){
+            next(error);
     }
 }
 
-const sendTokenResponse = async (user, codeStatus, res) => {
+const sendTokenResponse = async (user, codeStatus, res)=>{
     const token = await user.getJwtToken();
     res
-        .status(codeStatus)
-        .cookie('token', token, { maxAge: 60 * 60 * 1000, httpOnly: true })
-        .json({
-            success: true,
-            role: user.role
-        })
+    .status(codeStatus)
+    .cookie('token', token, {maxAge: 60*60*1000, httpOnly: true})
+    .json({success: true, token, user})
 }
 
 
-// log out
-exports.logout = (req, res, next) => {
+//Logout feature
+
+exports.logout = (req, res, next) =>{
     res.clearCookie('token');
     res.status(200).json({
-        success: true,
-        message: "logged out"
+        success : true,
+        message: "Logged out Successfully"
     })
 }
+//User profile
 
-
-// user profile
-exports.userProfile = async (req, res, next) => {
-
-    const user = await User.findById(req.user.id).select('-password');
-
+exports.userProfile = async(req, res, next) => {
+   const user = await User.findById(req.user.id).select('-password');
     res.status(200).json({
-        success: true,
+        success : true,
         user
     })
 }
