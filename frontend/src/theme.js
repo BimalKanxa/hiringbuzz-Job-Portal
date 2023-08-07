9828f97e095de3b6b8ab315c7515f9c521260845
BimalKanxa <sharmabimal2003@gmail.com>
2023-08-07 20:05:03 +0530

    no commit

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,10 +1,10 @@
 import {createTheme} from '@mui/material/styles';
-import {blue, lightBlue} from '@mui/material/colors';
+import {blue, lightBlue, red} from '@mui/material/colors';
 
 export const theme = createTheme({
     palette :{
         primary:{
-            main: blue[500]
+            main:red[500]     //blue
         },
         secondary :{
             main: lightBlue[800],
