4103ebf15fa78dd228a67ba818224b1fb45e2aa4
BimalKanxa <sharmabimal2003@gmail.com>
2023-12-05 08:01:23 +0530

    added analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,6 +22,7 @@ import DashJobs from './pages/admin/DashJobs';
 import DashCategory from './pages/admin/DashCategory';
 import DashCreateJob from './pages/admin/DashCreateJob';
 import DashCreateCategory from './pages/admin/DashCreateCategory';
+import { Analytics } from '@vercel/analytics/react';
 
 //HOC -> higher order component
 const UserDashboardHOC = Layout(UserDashboard);
@@ -80,6 +81,7 @@ const App = () => {
                     </BrowserRouter>
                 </ProSidebarProvider>
             </ThemeProvider>
+            <Analytics/>
         </>
     )
 }
